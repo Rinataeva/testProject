9dca3dd8349d62f070eeb561f5e4f9258404ff7d
Rinataeva <rinataeva@gmail.com>
2024-12-27 17:37:31 +0300

    added home-page cta

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,26 +1,32 @@
 import { Link } from "react-router";
 import { useNavigate } from "react-router";
 import { ROUTES } from "../../routes/routes.js";
-import { Table } from "../Table/Table.jsx";
+import { Table } from "../../components/Table/Table.jsx";
 import "./styles.css";
 
 export const Home = () => {
   const navigate = useNavigate();
 
   const goToCardsHandler = async () => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
     navigate(ROUTES.cards);
   };
 
   return (
     <div className="home-page">
-      <h3></h3>
+      <h2>Be wild in learning</h2>
+      <h3>as in conversation</h3>
       <p>
-        <Link to={ROUTES.cards}>Cards</Link>
+        <Link to={ROUTES.cards} className="home-page__link">Get started</Link>
       </p>
       <Table cards={[]} />
-      <button className="home-page__go-to-cards-button" onClick={goToCardsHandler}>Go to cards</button>
+      <button
+        className="home-page__go-to-cards-button"
+        onClick={goToCardsHandler}
+      >
+        Go to cards
+      </button>
     </div>
   );
 };
