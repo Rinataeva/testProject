f6e552e409c995517e4cdcbb5ab5fb59f06579dd
Rinataeva <rinataeva@gmail.com>
2024-12-27 17:31:03 +0300

    changed approutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,12 +4,12 @@ import { Cards } from "../pages/Cards/Cards.jsx";
 import { Card } from "../pages/Card";
 import { NotFound } from "../pages/NotFound";
 import { ROUTES } from "./routes.js";
-import { Table } from "../pages/Table/Table.jsx";
+// import { Table } from "../components/Table/Table.jsx";
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      <Route index element={ROUTES.home ? <Home /> : <Table />} />
+      <Route index element={ROUTES.home ? <Home /> : null} />
       <Route path={ROUTES.cards} element={<Cards />} />
       <Route path={ROUTES.card} element={<Card />} />
       <Route path="*" element={<NotFound />} />
