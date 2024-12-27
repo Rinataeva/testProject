6b5a69f4745f497208cfe916e45c5dbe678deef5
Rinataeva <rinataeva@gmail.com>
2024-12-27 18:18:34 +0300

    moved Navigation to components

--- src/components/Layout/components/Navigation/Navigation.jsx
+++ src/components/Layout/components/Navigation/Navigation.jsx
@@ -2,10 +2,10 @@ import { Link } from "react-router";
 import { navLinks } from "./navLinks";
 import "./styles.css";
 import { useContext } from "react";
-import { CounterContext } from "../../../../../context/CounterContext";
+import { CounterContext } from "../../../../context/CounterContext";
 
 export function Navigation() {
-  const {counter} = useContext(CounterContext);
+  const { counter } = useContext(CounterContext);
   return (
     <nav className="navigation">
       {navLinks.map(({ link, title }, index) => (
---
+++ src/components/Layout/components/Navigation/navLinks.js
@@ -0,0 +1,15 @@
+import { ROUTES } from "../../../../routes/routes.js";
+export const navLinks = [
+  {
+    link: ROUTES.home,
+    title: "Home",
+  },
+  {
+    link: ROUTES.cards,
+    title: "Cards",
+  },
+  {
+    link: ROUTES.logIn,
+    title: "Log in",
+  },
+];
