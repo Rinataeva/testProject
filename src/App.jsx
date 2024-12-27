226dab38d9924ff3a2d0e779e3bd9f8291064876
Rinataeva <rinataeva@gmail.com>
2024-12-27 17:46:30 +0300

    added useCounter and CounterContext.Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import { BrowserRouter } from "react-router";
 import { AppRoutes } from "./routes/AppRoutes";
 import { Layout } from "./components/Layout/Layout";
+import { CounterContext } from "./context/CounterContext";
+import { useCounter } from "./hooks/useCounter";
 
 
 function App() {
+  const value = useCounter();
   return (
     <BrowserRouter>
+    <CounterContext.Provider value={value}>
       <Layout>
         <AppRoutes />
       </Layout>
+      </CounterContext.Provider>
     </BrowserRouter>
   );
 }
