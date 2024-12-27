e159c727dbad7566d06a7f8c98eb08d15e1871cb
Rinataeva <rinataeva@gmail.com>
2024-12-27 17:39:18 +0300

    feat/onFlip prop to use counter in parent component

--- src/pages/Card/Card.jsx
+++ src/pages/Card/Card.jsx
@@ -2,21 +2,23 @@
 import { useState } from "react";
 import "./styles.css";
 
-export const Card = ({ english, transcription, russian }) => {
+export const Card = ({ english, transcription, russian, onFlip }) => {
   const [flipped, setFlipped] = useState(false);
+  
 
   const handleCardClick = () => {
     setFlipped(!flipped);
+    onFlip();
   };
-
+  
   return (
     <div
       className={`card ${flipped ? 'card--flipped' : ''}`} onClick={handleCardClick}
     >
       <div className="card__inner">
         <div className="card__front">
-          <h2>{english}</h2>
-          <p className="transcription">{transcription}</p>
+          <h3>{english}</h3>
+          <p className="card__transcription">{transcription}</p>
         </div>
         <div className="card__back">
           <h2>{russian}</h2>
--- src/pages/Cards/Cards.jsx
+++ src/pages/Cards/Cards.jsx
@@ -3,11 +3,14 @@ import { ROUTES } from "../../routes/routes.js";
 import { Card } from "../Card/Card.jsx";
 import { useState, useEffect } from "react";
 import { cardApiService } from "../../ApiService/ApiService.js";
+import { CounterContext } from "../../context/CounterContext";
+import { useContext } from "react";
 import "./styles.css";
 
 export const Cards = () => {
   const [words, setWords] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0); 
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { addWord } = useContext(CounterContext);
 
   useEffect(() => {
     fetchWords();
@@ -34,30 +37,44 @@ export const Cards = () => {
     }
   };
 
-  return (
+    const onFlip = () => {
+    addWord(words[currentIndex].english);
+  };
 
+  return (
     <section className="cards-section">
       <div className="progress">
         {currentIndex + 1}/{words.length}
       </div>
       <div className="cards-swiper">
-        <button className="cards-swiper__button" onClick={handleBackwardClick} disabled={currentIndex === 0}>
+        <button
+          className="cards-swiper__button"
+          onClick={handleBackwardClick}
+          disabled={currentIndex === 0}
+        >
           {"<-"}
         </button>
-        {words.length > 0 && <Card 
-        english={words[currentIndex].english}
-        transcription={words[currentIndex].transcription}
-        russian={words[currentIndex].russian} />}
-        
-        <button className="cards-swiper__button"
+        {words.length > 0 && (
+          <Card
+            english={words[currentIndex].english}
+            transcription={words[currentIndex].transcription}
+            russian={words[currentIndex].russian}
+            onFlip={onFlip}
+          />
+        )}
+
+        <button
+          className="cards-swiper__button"
           onClick={handleForwardClick}
           disabled={currentIndex === words.length - 1}
         >
           {"->"}
         </button>
       </div>
-      
-      <Link to={ROUTES.home}>Back</Link>
+      <div className="cards__footer">
+        <Link to={ROUTES.home}>Back</Link>
+        <Link to={ROUTES.card}>Show the card</Link>
+      </div>
     </section>
   );
 };
