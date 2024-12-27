5987f00f4752136f97e7569ba186618159dd2753
Rinataeva <rinataeva@gmail.com>
2024-12-27 17:45:10 +0300

    added CounterContext

--- src/components/Layout/components/Header/Navigation/Navigation.jsx
+++ src/components/Layout/components/Header/Navigation/Navigation.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router";
 import { navLinks } from "./navLinks";
 import "./styles.css";
+import { useContext } from "react";
+import { CounterContext } from "../../../../../context/CounterContext";
 
 export function Navigation() {
+  const {counter} = useContext(CounterContext);
   return (
     <nav className="navigation">
       {navLinks.map(({ link, title }, index) => (
@@ -10,6 +13,7 @@ export function Navigation() {
           {title}
         </Link>
       ))}
+      <div>Progress: {counter} words</div>
     </nav>
   );
 }
