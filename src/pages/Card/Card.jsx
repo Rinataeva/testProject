6c144413d19497247165148a98c639b743199a7b
Rinataeva <rinataeva@gmail.com>
2025-01-12 23:48:14 +0300

    feat/useRef

--- src/pages/Card/Card.jsx
+++ src/pages/Card/Card.jsx
@@ -1,19 +1,26 @@
 /* eslint-disable react/prop-types */
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
 export const Card = ({ english, transcription, russian, onFlip }) => {
   const [flipped, setFlipped] = useState(false);
-  
+  const seeTranslationButtonRef = useRef(null);  
 
   const handleCardClick = () => {
     setFlipped(!flipped);
     onFlip();
   };
-  
+
+  useEffect(() => {
+        if (seeTranslationButtonRef.current) {
+      seeTranslationButtonRef.current.focus();
+    }
+  }, []); 
+
   return (
     <div
-      className={`card ${flipped ? 'card--flipped' : ''}`} onClick={handleCardClick}
+      className={`card ${flipped ? "card--flipped" : ""}`}
+      onClick={handleCardClick}
     >
       <div className="card__inner">
         <div className="card__front">
@@ -24,6 +31,13 @@ export const Card = ({ english, transcription, russian, onFlip }) => {
           <h2>{russian}</h2>
         </div>
       </div>
+      <button
+        ref={seeTranslationButtonRef}  
+        className="see-translation-btn"
+        onClick={handleCardClick}
+      >
+       Click to see translation
+      </button>
     </div>
   );
 };
